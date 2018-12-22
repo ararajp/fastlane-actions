f56bb4f54863b5391701913aba1527904424f859
ararajp <ararajp@gmail.com>
2018-12-22 14:13:37 +0900

    add project

---
+++ FastlaneActions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastlaneActions
+//
+//  Created by Yusuke Arai on 2018/12/22.
+//  Copyright © 2018年 Yusuke Arai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
